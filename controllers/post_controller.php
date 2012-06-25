1088a0d4ee6842dc75e0e8f8b1fc705bd6c78772
Rajesh Kumar <rajesh@meetrajesh.com>
2012-06-25 01:22:58 -0400

    Finish renaming

--- controllers/post_controller.php
+++ controllers/post_controller.php
@@ -5,7 +5,7 @@ class PostController extends BaseController {
     public function add() {
         if (!empty($_POST['content'])) {
             csrf::check();
-            post::add($_POST['content'], session::current_user_id());
+            post::add($_POST['content'], session::cuser_id());
         }
         redirect(PATH_PREFIX);
     }
