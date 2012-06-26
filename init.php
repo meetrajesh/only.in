b4f4a101410f19a090b4d95adc197f53f2966f37
Rajesh Kumar <rajesh@meetrajesh.com>
2012-06-25 21:17:57 -0400

    adding gitignore and moving init.php stuff to init.lcaol.php

--- init.php
+++ init.php
@@ -4,23 +4,14 @@ ini_set('error_reporting', E_ALL | E_STRICT);
 ini_set('display_errors', true);
 ini_set('html_errors', true);
 
-session_start();
-
-define('BASE_URL', 'http://onlyin.com');
-define('PATH_PREFIX', ''); // eg. /onlyin
 define('CSRF_SECRET', 'm2*xb23)./2nej3{3$');
 define('USER_SALT_LEN', 3);
 define('USER_PWD_SECRET', '20)(*2mn3h32119**@#21');
 
-// database credentials
-define('DBHOST', 'localhost');
-define('DBUSER', '');
-define('DBPASS', '');
-define('DBNAME', 'onlyin');
+session_start();
+date_default_timezone_set('America/New_York');
 
 require './functions.php';
-
-date_default_timezone_set('America/New_York');
 
 // clean up $_GET and $_POST, ensure all values are strings (no arrays)
 foreach (array('_GET', '_POST', '_REQUEST') as $sglobal) {
