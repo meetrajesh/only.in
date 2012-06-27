e84a9ac50c7d8034704fc1a8ad27461c78a15e22
Rajesh Kumar <rajesh@meetrajesh.com>
2012-06-27 14:34:18 -0400

    Creating IS_DEV constant for dev mode

--- init.local.sample.php
+++ init.local.sample.php
@@ -3,6 +3,7 @@
 define('BASE_URL', ''); // eg. http://only.in.dev
 define('API_BASE_URL', ''); // eg. http://api.only.in.dev
 define('PATH_PREFIX', ''); // eg. /onlyin
+define('IS_DEV', true);
 
 // database credentials
 define('DBHOST', 'localhost');
