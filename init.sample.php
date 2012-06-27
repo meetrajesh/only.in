555c10515eaf0833e54153309ce6d626e6e07f84
Rehan Dalal <rehandalal@gmail.com>
2012-06-27 06:35:16 -0400

    Add a sample init file

---
+++ init.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASE_URL', ''); // eg. http://only.in.dev
+define('PATH_PREFIX', ''); // eg. /onlyin
+
+// database credentials
+define('DBHOST', 'localhost');
+define('DBUSER', '');
+define('DBPASS', '');
+define('DBNAME', 'onlyin');
