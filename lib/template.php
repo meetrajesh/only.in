baf31ff1f110ae9f3c2683a192d709e9bd8608c4
Rehan Dalal <rehandalal@gmail.com>
2012-06-27 15:59:55 -0400

    Implement basic templating

---
+++ lib/template.php
@@ -0,0 +1,27 @@
+<?php
+
+class template {
+
+    protected $_block;
+    protected $_blocks = array();
+
+    public function block($name) {
+        $this->_block = $name;
+        ob_start();
+    }
+
+    public function endblock($output = false) {
+        $content = ob_get_clean();
+
+        if (!isset($this->_blocks[$this->_block])) {
+            $this->_blocks[$this->_block] = $content;
+        }
+
+        if ($output) {
+            echo $this->_blocks[$this->_block];
+        }
+
+        $this->_block = null;
+    }
+
+}
