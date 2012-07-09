bf2c5d1a83478691ffb6c6f9ed262e56ddcf8e42
Rehan Dalal <rehandalal@gmail.com>
2012-07-09 19:54:00 -0400

    automatically define WEB_ROOT

--- init.php
+++ init.php
@@ -18,6 +18,7 @@ define('API_SECRET', '95dbb8238195850');
 define('IMGUR_API_KEY', 'a101ec6dba513b6e8a0ad48c7a5a65f0');
 define('UPLOAD_MAX_SIZE', 10*1024*1024); // 10M
 define('STATIC_PREFIX', '/s');
+define('WEB_ROOT', dirname(__FILE__) . '/public');
 
 session_start();
 date_default_timezone_set('America/New_York');
