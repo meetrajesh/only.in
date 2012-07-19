99422068eade5b8a4baa282daeae9b55b1166eb8
Rajesh Kumar <rajesh@meetrajesh.com>
2012-07-19 00:13:25 -0400

    Fix Content-Type header case

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
 require '../init.php';
-header('Content-type: application/json');
+header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: ' . BASE_URL);
 
 echo json_encode(IndexController::api_route());
